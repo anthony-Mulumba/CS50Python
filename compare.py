25002dd860b5827b342c901eef2cf8d224acdafc
unknown <anthoxbest@gmail.com>
2022-07-16 23:15:45 +0200

    Adding compare function

---
+++ compare.py
@@ -0,0 +1,10 @@
+x= int(input("enter the value of x : "))
+y= int(input("enter thevalue of y : "))
+
+if x > y:
+    print(f"{x} is greater than {y}")
+elif x < y:
+    print(f"{x} is less than {y}")
+elif x == y:
+    print(f"{x} equal {y}")
+
