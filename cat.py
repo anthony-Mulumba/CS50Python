7a350c96f46c4a5be7fe410da62349a4f3bfd1b5
unknown <anthoxbest@gmail.com>
2022-07-17 15:59:50 +0200

    Add new function List named Hogwarts.py

--- cat.py
+++ cat.py
@@ -1,3 +1,22 @@
+def main():
+    number = get_number()
+    meow(number)
+
+#Funtion to get a number
+def get_number():
+    while True:
+        n=int(input("Enter a positive number: "))
+        if n > 0:
+            break
+    return n
+
+#Funtion to print meow n times
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()
+
 #Loop
 #i = 3
 #while i!= 0:
@@ -8,10 +27,10 @@
 
 #Infinite loop until a condition is made to break it
 
-while True:
-    n=int(input("Enter a positive number: "))
-    if n > 0:
-        break
+#while True:
+ #   n=int(input("Enter a positive number: "))
+  #  if n > 0:
+   #     break
 
-for _ in range(n):
-    print("meow")
+#for _ in range(n):
+ #   print("meow")
