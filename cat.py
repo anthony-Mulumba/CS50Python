06e5ffc6f9e0dfb343ab1573aed96e11b6d14cb2
unknown <anthoxbest@gmail.com>
2022-07-17 15:43:56 +0200

    Adding Loop function cat.py

---
+++ cat.py
@@ -0,0 +1,17 @@
+#Loop
+#i = 3
+#while i!= 0:
+ #   print("meow")
+  #  i = i-1
+#print("\n")
+#print("mewo \n" * 3, end="")
+
+#Infinite loop until a condition is made to break it
+
+while True:
+    n=int(input("Enter a positive number: "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
