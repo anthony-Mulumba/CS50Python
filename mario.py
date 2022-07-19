4fe03bc0438a26d6557c0461088f78fa9c7b8ba7
unknown <anthoxbest@gmail.com>
2022-07-19 18:43:34 +0200

    Update mario.py

--- mario.py
+++ mario.py
@@ -2,11 +2,20 @@ def main():
     x= int(input("Enter the number of time: "))
     print_briks(x)
 
-def print_briks(n):
+#def print_briks(n):
     #Print n briks in a row n time.
-    for i in range(n):
-        for j in range(n):
-            print("#", end="")
-        print()
+ #   for i in range(n):
+  #      for j in range(n):
+   #         print("#", end="")
+    #    print()
+
+def print_briks(size):
+    #Print n briks in a row n time.
+    for i in range(size):
+        print_row(size)
+
+def print_row(width):
+    print("#" * width)
+
 
 main()
