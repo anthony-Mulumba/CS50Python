a85c3c534fda960b15f5f9333112bc51f2deacd2
unknown <anthoxbest@gmail.com>
2022-07-21 23:01:47 +0200

    Updated number.py to enter three times wrong number and get out

--- number.py
+++ number.py
@@ -1,16 +1,23 @@
+from msilib import type_string
+
+
 def main():
     x = get_int_pass("What is the number? ")
-    print(f"You have entered: {x}")
+    print(f"{x}")
 
 
-#Here we don't prompt to the user the error faced but ordered him to enter again a number
-def get_int_pass(prompt):
-    while True:
+#Here we don't prompt to the user the error faced but ordered him to enter again a number 3 times
+#n=3 is a default value
+def get_int_pass(prompt, n=3):
+    value=1
+    while value <= n:
         try:
             return int(input(prompt))
         except ValueError:
+            value +=1
             pass
-
+    return "You have entered three time non integer "
+    
 #Here the user received an error code when entering a non interger number
 def get_int():
     while True:
