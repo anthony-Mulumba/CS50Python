bc3cf4f94663d8553be8599ea7ddd482ecfd35e8
unknown <anthoxbest@gmail.com>
2022-08-01 18:51:06 +0200

    Update heelo.py for jenkins

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def main():
-    name = input("What is your name? ").strip().title()
+    #name = input("What is your name? ").strip().title()
     hello(name)
 
 def hello(to="world"):
---
+++ name.py
@@ -0,0 +1,4 @@
+#This Module help to get user input on the command line when executing the program
+import sys
+
+print("Hello my name is ", sys.argv[1], sys.argv[2])
